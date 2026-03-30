e7db13b8b0e87e60f7d44d8dd2b3337ed70412b9
Hoang Do Khai <khai.hoangdo@ncc.asia>
2026-03-30 13:41:31 +0700

    Remove old duplicate files from root after folder restructure

--- churn_feature_store/churn-feature/feature_repo/data_sources.py
+++
@@ -1,9 +0,0 @@
-from feast import FileSource
-from datetime import datetime
-
-customer_stats_source = FileSource(
-    name="customer_stats_source",
-    path="data/processed_churn_data.parquet",
-    timestamp_field="event_timestamp",
-    created_timestamp_column="created_timestamp",
-)
